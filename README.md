300c929f055386121a035f1e20967a52581c0df2
Mysta3 <myshawnestallings@gmail.com>
2021-02-22 21:37:01 -0800

    exercises 1 added

---
+++ README.md
@@ -0,0 +1,12 @@
+# Exercises
+## Description
+- used for student to complete exercises that will help them retain and gain a deeper understanding of certain technologies.
+
+## Instructions
+- Instructions for each exercise will be commented out in the associated file.
+- Please complete by using PR's to add your code to this repo
+- I will merge unless stated otherwise.
+- In case of a session being cancelled I will continue to post exercises to make up for the session.
+
+## For Questions
+- For questions please google first before asking as I want to teach you real world techniques and habits.
---
+++ intro/script.js
@@ -0,0 +1,21 @@
+// Objective: Successfully create a hello world project and push it to github
+// Assignment: Create a PR that contains the following changes -> Add html file, Add css file. Connect html with css and this js file. Complete javascript questions. Create a PR with your changes once you are done.
+
+// Get Started:
+// 1. Clone this repo
+// 2. Make your edits
+// 3. Create a PR request.
+
+// HINT: here are some git commands that will be useful 
+// GIT COMMANDS
+// git clone, git checkout -b your_branch_name, git add, git commit -m "YOUR COMMIT MESSAGE HERE", git push origin your_branch_name
+
+// Create 1 of each type of variable
+// Integer
+// String
+// Array
+// Object 
+// console log each variable
+
+// BONUS
+// can you log each variable using a loop?
